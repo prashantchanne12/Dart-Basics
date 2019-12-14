625efb363265d702f508e860cb6f7eb1c03e3d69
prashant <prashant121@gmail.com>
2019-12-14 21:17:52 +0530

    first commit

---
+++ 1.dart
@@ -0,0 +1,33 @@
+void main() {
+//   print("Hello World");
+//   print("Mr Robot");
+
+  // This is comments
+
+  /*
+    Multiline
+    comment
+  */
+
+  // Various data types that Dart supports
+
+  // 1. Numbers - int, double
+  // 2. Booleans
+  // 3. Strings
+  // 4. Lists
+  // 5. Maps
+  // 6. Runes
+  // 7. Symbols
+
+  // All the data types in are Objects
+  // Therefore, Values are by default null
+
+  // 2 types of declaring the variable
+
+  int age = 10;
+  // or
+  var a = 10;
+
+  print(age);
+  print(a);
+}
