35671715c2654fbdfba84c98da285b9126212e70
prashant <prashant121@gmail.com>
2019-12-15 00:12:28 +0530

    iterations in Dart

---
+++ 6.dart
@@ -0,0 +1,35 @@
+void main(){
+  
+  // Iterations
+  
+  
+  // For Loop
+  var sum = 0;
+  
+  for(var i=1; i<=5; i++){
+    sum += i;
+  }
+  
+  print(sum);
+  
+  
+  // While Loop
+  var a = 5;
+  var b = 0;
+  
+  while(a!=0){
+    b += a;
+    a--;
+  }
+  
+  print(b);
+  
+  // Do While loop
+  
+  var r = 0;
+  
+  do{
+    print("Hello World!");
+  }while(r!=0);
+  
+}
