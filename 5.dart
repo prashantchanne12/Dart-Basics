bc7d26a126e113b7f7169c405280c35f57c087b4
prashant <prashant121@gmail.com>
2019-12-14 22:18:21 +0530

    switch statements

---
+++ 5.dart
@@ -0,0 +1,26 @@
+void main(){
+  
+  // Applicable only for 'int' and 'String'
+  
+  var grade = 'A';
+  
+  switch(grade){
+      
+    case 'A' : print("Well done boi");
+               break;
+      
+    case 'B' : print("Good my boi");
+               break;
+      
+    case 'C' : print("Okay Okay");
+               break;
+      
+    case 'D' : print("Are you kidding me?");
+               break;
+      
+    default : print("Come on Man!");
+              break;
+      
+  }
+  
+}
